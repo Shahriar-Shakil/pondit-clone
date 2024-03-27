1a9f7a3a4708b2c89a3bf0881c98f6b714a0a812
shahriar-shakil <toshahriarshakil@gmail.com>
2024-03-27 17:57:22 +0600

    working on our courses

---
+++ src/components/OurCourses.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function OurCourses({}: Props) {
+  return (
+    <div className="home-slide  new-course">
+      <div className="container max-w-2xl mx-auto px-2  pb-14"></div>
+    </div>
+  );
+}
